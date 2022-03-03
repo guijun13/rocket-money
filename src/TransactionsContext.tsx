fc88ab95c96af54d8f57d084d4c7b3b4e61d69b2
guijun13 <guilhermejun13.grillo@gmail.com>
2022-03-02 23:05:22 -0300

    feat: add modal data to table using context fns

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -20,7 +20,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -34,8 +34,14 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     api.get('/transactions').then((response) => setTransactions(response.data.transactions));
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction);
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]); // immutability
   }
 
   return (
